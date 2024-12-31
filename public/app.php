86dac6c85057d519134c1f70572f0ccf91e55f00
Garvin Hicking <blog@garv.in>
2024-12-31 01:03:53 +0100

    [TASK] Rename entrypoint

--- public/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-use GarvinHicking\Gerrviewn\Core;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = new Core();
-$app->run();
--- public/index.php
+++ public/index.php
@@ -1,53 +1,9 @@
 <?php
-function render_table(string $query): void {
-    $database = new SQLite3('../db/db.sqlite');
-    $results = $database->query($query);
+declare(strict_types=1);
 
-    echo "<table border='1'>";
+use GarvinHicking\Gerrviewn\Core;
 
-    $has_head = false;
-    while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-        if (!$has_head) {
-            echo '<thead><tr>';
-            foreach($row as $key => $_) {
-                echo '<th>' . $key . '</th>';
-            }
-            echo '</tr></thead>';
-            echo '<tbody>' . "\n";
-        }
-        $has_head = true;
-        echo "<tr>";
+require __DIR__ . '/../vendor/autoload.php';
 
-        foreach($row as $key => $value) {
-            echo "<td>" . nl2br(htmlspecialchars($value ?? '')) . "</td>" . "\n";
-        }
-
-        echo "</tr>";
-    }
-
-    echo '</tbody>';
-    echo "</table>";
-    $database->close();
-}
-?>
-<html>
-
-<body>
-<?php
-
-echo "<h1>Gerrit patches ON MAIN</h1>";
-render_table('SELECT * FROM changes WHERE branch = "main" AND is_active = 1');
-
-echo "<h1>Gerrit patches cherry-picks</h1>";
-render_table('SELECT * FROM changes WHERE branch != "main" AND is_active = 1');
-
-echo "</body></html>";
-
-/**
- * TODO:
-- Wie zusammengehörige Patches für unterschiedliche Branches mergen?
-- Core-Owner andere Farbe
-- Regulars (Leute wie Torben) andere Farbe
-
-Title | WIP? | Owner | Involved People | Branch | Created / Last Modified / Age | Votes
-*/
+$app = new Core();
+$app->run();
