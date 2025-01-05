1192bf84a50764287d02a5422c2b40c179e735b7
Garvin Hicking <blog@garv.in>
2025-01-06 00:36:22 +0100

    [TASK] Add cronjob stub

---
+++ json/cron.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+use GarvinHicking\Gerrviewn\Core;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// TODO: I want to be a Symfony Console script.
+$app = new Core();
+$app->cliFetch();
+$app->cliParse();
